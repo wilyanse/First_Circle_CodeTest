44e3564585ea43bba90bfe894c34c0c030a0793b
Wilyanse <willjose51@gmail.com>
2024-07-20 13:52:42 +0800

    processing: finish processing

---
+++ processing.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import glob
+import os
+
+from processor import Processor
+
+def find_csv_files(directory):
+    patterns = {
+        'pricing-*.csv': [],
+        'transactions-*.csv': [],
+        'users-*.csv': []
+    }
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            for pattern in patterns.keys():
+                if glob.fnmatch.fnmatch(file, pattern):
+                    file_path = os.path.join(root, file)
+                    patterns[pattern].append(file_path)
+    
+    return patterns
+
+def process_files(patterns, process_users, process_transactions, process_pricing):
+    for pattern, files in patterns.items():
+        for file_path in files:
+            if pattern == 'pricing-*.csv':
+                print(f'Processing pricing file: {file_path}')
+                process_pricing.add_csv(file_path)
+                
+            elif pattern == 'transactions-*.csv':
+                print(f'Processing transactions file: {file_path}')
+                process_transactions.add_csv(file_path)
+                
+            elif pattern == 'users-*.csv':
+                print(f'Processing users file: {file_path}')
+                process_users.add_csv(file_path)
+
+def correct_amount(process_transactions, process_pricing):
+    unique_pricing = process_pricing.dataframe.loc[process_pricing.dataframe.groupby(['product'])['puk'].idxmax()]
+    merged_df = pd.merge(process_transactions.dataframe, unique_pricing, on='product')
+    desired_cols = ['trans_id', 'user_id', 'product', 'price', 'trans_date']
+    merged_df = merged_df[desired_cols]
+    merged_df = merged_df.rename(columns={'price': 'amount'})
+
+    process_transactions.dataframe = merged_df
+    process_transactions.clean_data(process_transactions.dataframe)
+    process_transactions.type_check(process_transactions.dataframe)
+
+def main():
+    directory = r'1_DataSources'
+    patterns = find_csv_files(directory)
+    columns = [
+        ['user_id', 'name', 'email', 'date_joined'],
+        ['trans_id', 'user_id', 'product', 'amount', 'trans_date'],
+        ['puk', 'product', 'price']
+    ]
+    types = [
+        ['int', 'str', 'str', 'datetime'],
+        ['int', 'int', 'str', 'int', 'datetime'],
+        ['int', 'str', 'int']
+    ]
+    process_users = Processor(columns[0], types[0])
+    
+    process_transactions = Processor(columns[1], types[1])
+    process_pricing = Processor(columns[2], types[2])
+    process_files(patterns, process_users, process_transactions, process_pricing)
+    correct_amount(process_transactions, process_pricing)
+
+if __name__ == "__main__":
+    main()
---
+++ processor.py
@@ -0,0 +1,62 @@
+import pandas as pd
+class Processor:
+    def __init__(self, columns, types):
+        self.dataframe = pd.DataFrame(columns=columns)
+        self.columns = columns
+        self.types = types
+    
+    def add_csv(self, filepath):
+        new_entries = pd.read_csv(filepath)
+        try:
+            new_entries = self.clean_data(new_entries)
+            new_entries = new_entries[self.columns]
+            self.dataframe = (self.dataframe.copy() if new_entries.empty else new_entries.copy() if self.dataframe.empty
+                                else pd.concat([self.dataframe, new_entries], ignore_index=True)
+                                )
+        except KeyError as e:
+            print("insufficient columns")
+
+    def type_check(self, df):
+            new_df = df.copy()
+            new_df['errors'] = new_df.apply(self.check_entries, axis=1)
+            errors_df = new_df[new_df['errors'].apply(lambda x: bool(x))]
+            if not errors_df.empty: print(errors_df[[self.columns[0], 'errors']])
+    
+    def check_entries(self, row):
+        errors = []
+        for cur in range(len(self.columns)):
+            if self.types[cur] == 'datetime':
+                if pd.isna(row[self.columns[cur]]):
+                    errors.append(str(self.columns[cur]) + ' is not datetime')
+            elif self.types[cur] == 'int':
+                if not isinstance(row[self.columns[cur]], int):
+                    if self.columns[cur] == -1:
+                        errors.append(str(self.columns[cur]) + ' has an incorrect data type')
+                        continue
+                    errors.append(str(self.columns[cur]) + ' is not ' + str(self.types[cur]))
+            else:
+                if not isinstance(row[self.columns[cur]], str):
+                    errors.append(str(self.columns[cur]) + ' is not ' + str(self.types[cur]))
+        return errors
+
+    def clean_data(self, df):
+        new_df = df
+
+        for cur in range(len(self.columns)):
+            if self.types[cur] == 'int':
+                new_df[self.columns[cur]] = pd.to_numeric(new_df[self.columns[cur]], errors='coerce', downcast='integer')
+                if self.columns[cur] == 'amount':
+                    new_df[self.columns[cur]] = new_df[self.columns[cur]].fillna(-1).astype(int)
+            elif self.types[cur] == 'datetime':
+                new_df[self.columns[cur]] = pd.to_datetime(new_df[self.columns[cur]], errors='coerce', format='mixed')
+            else:
+                new_df[self.columns[cur]] = new_df[self.columns[cur]].astype(str)
+    
+        new_df = self.drop_nulls(new_df)
+        return new_df
+
+    def get_dataframe(self):
+        return self.dataframe
+
+    def drop_nulls(self, df):
+        return df.dropna()
